bd54d11f2036ee60bb1b31736d0356ce4db28265
gw <kuvykin@lsrm.ru>
2019-12-25 13:10:00 +0300

    - add thread-safe unordered_map - guard with mutex work with bulk: believed to be thread-safe

--- async.cpp
+++ async.cpp
@@ -1,57 +1,55 @@
 #include "async.h"
 
-#include <vector>
 #include <utility>
-#include <shared_mutex>
+#include <mutex>
 
 #include "bulk.h"
 #include "command_handler.h"
 #include "command_processor.h"
+#include "ts_cont.h"
 
 using namespace std;
 
 namespace async {
 
-vector<unique_ptr<CommandProcessor>> bulkmgrs;
-shared_mutex vec_mtx;
+ThreadSafeUnorderedMap<unique_ptr<CommandProcessor>> bulkmgrs;
+mutex mtx;
 
 handle_t connect(std::size_t bulk_size) {
     auto dataProcessor = make_unique<CommandProcessor>(bulk_size);
     createObserverAndSubscribe<CmdStreamHandler>(dataProcessor->getBulkMgr().get());
     createObserverAndSubscribe<CmdFileHandler>(dataProcessor->getBulkMgr().get());
-    unique_lock<shared_mutex> lk(vec_mtx);
-    bulkmgrs.push_back(move(dataProcessor));
-    return reinterpret_cast<void*>(bulkmgrs.size()-1);
+    return reinterpret_cast<void*>(
+            bulkmgrs.push(move(dataProcessor))
+    );
 }
 
 void receive(handle_t handle, const char *data, std::size_t size) {
     auto idx = reinterpret_cast<size_t>(handle);
-    shared_lock<shared_mutex> lk(vec_mtx);
     auto& dataProcessor = *bulkmgrs[idx];
-    lk.unlock();
     auto& cmdReader = dataProcessor.getcmdReader();
     auto& bulkMgr = dataProcessor.getBulkMgr();
+    lock_guard<mutex> lk(mtx);
     dataProcessor.pushToBuffer(data, size);
     process_all_commands(cmdReader, *bulkMgr);
 }
 
 void disconnect(handle_t handle) {
     auto idx = reinterpret_cast<size_t>(handle);
-    shared_lock<shared_mutex> lk(vec_mtx);
     auto& dataProcessor = *bulkmgrs[idx];
-    lk.unlock();
     auto& buffer = dataProcessor.getBuffer();
     auto& cmdReader = dataProcessor.getcmdReader();
     auto& bulkMgr = dataProcessor.getBulkMgr();
-    if (!buffer.empty()) {
-        auto cmd = cmdReader.read_next_cmd();
+    {
+        lock_guard<mutex> lk(mtx);
+        if (!buffer.empty()) {
+            auto cmd = cmdReader.read_next_cmd();
+            bulkMgr->add_cmd(move(cmd));
+        }
+        Command cmd{CommandType::Terminator};
         bulkMgr->add_cmd(move(cmd));
     }
-    Command cmd{CommandType::Terminator};
-    lk.lock();
-    bulkMgr->add_cmd(move(cmd));
-    lk.unlock();
-    bulkmgrs[idx] = nullptr;
+    bulkmgrs.erase(idx);
 }
 
 }
